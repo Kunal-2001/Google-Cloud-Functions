b4cc4d2575d7657e77a8fd0fc0e4247cf2836404
Kunal-2001 <kunal999936@gmail.com>
2021-11-17 00:03:44 +0530

    API schema restructured

--- functions/index.js
+++ functions/index.js
@@ -2,32 +2,81 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const collectionRef = admin.firestore().collection("Staff");
+const orgCollectionRef = admin.firestore().collection("Organizations");
+
+exports.addOrganization = functions.https.onRequest(async (req, res) => {
+  try {
+    const organizationName = req.body;
+
+    const addedOrg = await orgCollectionRef.add({ Name: organizationName });
+
+    res.status(204);
+  } catch (err) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+});
+
+exports.deleteOrganization = functions.https.onRequest(async (req, res) => {
+  try {
+    const organizationID = req.params[0];
+    const organizationDocRef = orgCollectionRef.doc(organizationID);
+
+    const deletedOrg = await organizationDocRef.delete();
+
+    res.status(200).json({ msg: "Organization deleted" });
+  } catch (err) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+});
+
+exports.updateOrganization = functions.https.onRequest(async (req, res) => {
+  try {
+    const data = req.body;
+    const organizationID = req.params[0];
+    const organizationDocRef = orgCollectionRef.doc(organizationID);
+
+    const updatedOrg = await organizationDocRef.update(data);
+
+    res.status(200).json({ msg: "Organization updated" });
+  } catch (err) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+});
 
 exports.addEmployee = functions.https.onRequest(async (req, res) => {
   try {
+    let params = req.params[0].split("/").splice(1);
+
     const employeeData = {
       StaffName: req.body.StaffName,
       MobileNo: req.body.MobileNo,
       Role: req.body.Role,
     };
 
-    const addedData = await collectionRef.add(employeeData);
+    let organizationDocRef = orgCollectionRef.doc(params[0]);
+    let organizationStaffRef = organizationDocRef.collection("Staff");
+
+    const addedData = await organizationStaffRef.add(employeeData);
 
     res.status(204);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ msg: "Something failed" });
   }
 });
 
 exports.deleteEmployee = functions.https.onRequest(async (req, res) => {
   try {
-    const docID = req.params[0];
-    const docRef = collectionRef.doc(docID);
+    let params = req.params[0].split("/").splice(1);
 
-    const deletedTimestamps = await docRef.delete();
+    const organizationDocRef = orgCollectionRef.doc(params[0]);
+    const organizationStaffRef = organizationDocRef.collection("Staff");
 
-    res.status(200).json({ msg: "Data succesfully deleted" });
+    const employeeRef = organizationStaffRef.doc(params[1]);
+
+    const deletedTimestamps = await employeeRef.delete();
+
+    res.status(200).json({ msg: "Employee succesfully deleted" });
   } catch (err) {
     res.status(500).json({ msg: "Failed to delete the document" });
   }
@@ -35,11 +84,15 @@ exports.deleteEmployee = functions.https.onRequest(async (req, res) => {
 
 exports.updateEmployee = functions.https.onRequest(async (req, res) => {
   try {
-    const docID = req.params[0];
-    const docRef = collectionRef.doc(docID);
-    const userData = req.body;
+    let params = req.params[0].split("/").splice(1);
+    const data = req.body;
 
-    const updatedTimestamp = await docRef.update(userData);
+    const organizationDocRef = orgCollectionRef.doc(params[0]);
+    const organizationStaffRef = organizationDocRef.collection("Staff");
+
+    const employeeRef = organizationStaffRef.doc(params[1]);
+
+    const deletedTimestamps = await employeeRef.update(data);
 
     res.status(200).json({ msg: "Data succesfully updated" });
   } catch (err) {
